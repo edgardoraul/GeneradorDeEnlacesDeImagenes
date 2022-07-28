3ca5dcbf04f97b776801f19915fd26305ee3f007
Edgard_Roul <edgardo_raul@hotmail.com>
2022-07-28 14:18:46 -0300

    Incluso hay control si tiene o no tabla de talles. Falta lo de los colores.

--- Generador de Enlaces de Imagenes.vb
+++ Generador de Enlaces de Imagenes.vb
@@ -6,22 +6,20 @@ Sub GeneradorImagenes()
 
 ' Definiendo las variables
 Dim URL As String
-Dim CantH As Integer
-Dim CantM As Integer
 Dim CantS As Integer
-Dim HYM As Boolean
-Dim Tabla As Boolean
 Dim codigo As LongLong
 Dim ultimaFila As Integer
 Dim i As Integer
 Dim extension As String
 Dim imagenes As String
 Dim contador As Integer
+Dim tabla As String
 
 
 ' Rellenando la url
 URL = "https://rerda.com/imagenes/"
 extension = ".jpg"
+tabla = "/tabla" & extension
 
 ' Obteniendo la última fila
 Range("A1").Select
@@ -31,27 +29,39 @@ ultimaFila = Range(Selection, Selection.End(xlDown)).Count
 For i = 1 To ultimaFila
     
     ' PRODUCTO SIMPLE
-    If Cells(i, 2).Value = "simple" Then
+    If Cells(i, 2).Value = "simple" Or Cells(i, 2).Value = "variable" Then
         
         ' Asigando la cantidad de imágenes que tiene el producto
-        CantS = Cells(i, 10).Value
+        CantS = Cells(i, 8).Value
         contador = 1
         imagenes = ""
-
-        ' Se recorre el loop y se concatenan los enlaces
         Do While contador <= CantS
-            imagenes = imagenes & "," & URL & Cells(i, 5).Value & "/" & contador & extension
+            imagenes = imagenes & "," & URL & Cells(i, 6).Value & "/" & contador & extension
             contador = contador + 1
         Loop
-
-        ' Se extrae el primer caracter de la cadena, ya que es una coma y no es necesario.
-        Cells(i, 6).Value = Right(imagenes, Len(imagenes) - 1)
+                
+        ' Controlando si tiene tabla
+        If Cells(i, 9).Value = 1 Then
+            imagenes = imagenes & "," & URL & Cells(i, 6).Value & tabla
+        End If
         
-    End If
+        ' Insertando el resultado completo de las imágenes
+        Cells(i, 7).Value = Right(imagenes, Len(imagenes) - 1)
     
-    ' PRODUCTO CON FOTOS HOMBRE Y MUJER
-
-    ' FOTOS CON VARIANTES DE COLOR
+    
+    ' PRODUCTO VARIANTES DE TALLE
+    ElseIf Cells(i, 2).Value = "variation" And Cells(i, 4).Value = 1 Then
+        
+        ' Asigando la cantidad de imágenes que tiene el producto
+        CantS = Cells(i, 8).Value
+        contador = 1
+        imagenes = ""
+        Do While contador <= CantS
+            imagenes = imagenes & "," & URL & Cells(i, 6).Value & "/" & contador & extension
+            contador = contador + 1
+        Loop
+        Cells(i, 6).Value = Right(imagenes, Len(imagenes) - 1)
+    End If
 
 Next
 
